274b1240dfa7c01cd142f08d851271cdde030205
cinderblock <lesseravi@gmail.com>
2019-01-22 13:37:19 -0500

    chugging ahead

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,19 @@ class ArticlesController < ApplicationController
 
 
   def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+      @article = Article.new(article_params)
+    if @article.save 
+    
+      flash[:notice] = "Article was succefully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  
   end
-
-
-
+def show
+@article = Article.find(params[:id])
+end
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:article, :description)
