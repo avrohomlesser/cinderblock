bf99220f1f06f507badc2776eb38c5e37ddda85b
cinderblock <lesseravi@gmail.com>
2019-01-23 16:24:40 -0500

    added user table to user branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_162456) do
+ActiveRecord::Schema.define(version: 2019_01_23_203124) do
 
   create_table "articles", force: :cascade do |t|
     t.string "article"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_01_18_162456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "gangfungel"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
