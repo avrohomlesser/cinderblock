48682a87b3b09b262f954c8fd2aa970c49e79900
cinderblock <lesseravi@gmail.com>
2019-01-25 10:53:53 -0500

    added user edit cap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,29 @@ def new
 @user = User.new
 
 end
+def edit
 
+@user = User.find(params[:id])
+
+end
+
+def update
+
+@user = User.find(params[:id])
+
+if @user.update(user_params)
+
+flash[:success] = "Your account was updated successfully"
+
+redirect_to articles_path
+
+else
+
+render 'edit'
+
+end
+
+end
 def create
 
 @user = User.new(user_params)
